bce189cd668b1d3364fe084e62257bab3c1e5aaf
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-09 11:55:48 -0500

    Add constructor to AccountController

---
+++ AuthenticationAspNetIdentity/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AuthenticationAspNetIdentity.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+    }
+}
