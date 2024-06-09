373d80999d6067efbebdb5bcb553cf3e56c79726
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-09 11:02:53 -0500

    Add ApplicationUser class with properties

---
+++ AuthenticationAspNetIdentity/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationAspNetIdentity
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
